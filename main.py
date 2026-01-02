f8815146f309c74132677feae75bfbcae08e09df
Shekhar Singh <smehla147@gmail.com>
2026-01-02 12:17:01 +1100

    build the vecore store for  my resume

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from resume-chatbot!")
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+load_dotenv()
+os.environ["HF_TOKEN"] = os.getenv("HF_TOKEN")
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+loader = Docx2txtLoader("masterResume.docx")
+
+# Load the data
+data = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splitted_docs = text_splitter.split_documents(data)
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+# Create the vector store
+vectorstore = FAISS.from_documents(splitted_docs, embeddings)
+resutl  = vectorstore.similarity_search("What is the skills",k=1)
+retriver = vectorstore.as_retriever()
+llm = ChatGroq(
+    model="qwen/qwen3-32b",
+    temperature=0,
+    max_tokens=None,
+    reasoning_format="parsed",
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+r = llm.invoke("Hello")
+print(r)
 
 
-if __name__ == "__main__":
-    main()
